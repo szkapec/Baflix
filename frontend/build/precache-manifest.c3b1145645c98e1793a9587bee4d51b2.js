be1ade89b030ea393dbdbbea88460d67497edd19
szkapec <szkapec999@o2.pl>
2020-03-17 16:14:25 +0100

    make it better

---
+++ frontend/build/precache-manifest.c3b1145645c98e1793a9587bee4d51b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94757ba8be9757a33444e0480e7aa71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ded4c9ad888cf90bb785",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9855746577610dfec14d",
+    "url": "/static/js/2.cd71192a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.cd71192a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ded4c9ad888cf90bb785",
+    "url": "/static/js/main.01e39d74.chunk.js"
+  },
+  {
+    "revision": "7ad3f10275b3ee3c01a0",
+    "url": "/static/js/runtime-main.bb000f1d.js"
+  }
+]);
