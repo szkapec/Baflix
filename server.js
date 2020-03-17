4486992efeeb57dfd2d63f342470ad5e9b7bb576
szkapec <szkapec999@o2.pl>
2020-03-17 17:07:14 +0100

    poprawa

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.use('/users', Users)
 
 if(process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static('frontend/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
