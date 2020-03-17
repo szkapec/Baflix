d1d9f9030eaf1b81b95f4f6ef6db14dcdc9de546
szkapec <szkapec999@o2.pl>
2020-03-17 16:49:34 +0100

    finish

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ var express = require('express')
 var cors = require('cors')
 var bodyParser = require('body-parser')
 var app = express()
+const path = require('path');
 const mongoose = require('mongoose')
-var port = process.env.PORT || 5000
+
+
 
 app.use(bodyParser.json())
 app.use(cors())
@@ -25,6 +27,18 @@ mongoose
   
 var Users = require('./routes/Users')
 app.use('/users', Users)
+
+
+if(process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static('frontend/build'));
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
+var port = process.env.PORT || 5000
+
 
 app.listen(port, function() {
   console.log('Server is running on port: ' + port)
