5c05780423da1588198192884fffcdef5ca0278e
szkapec <szkapec999@o2.pl>
2020-04-02 22:48:13 +0200

    change axios3

--- routes/Twitter.js
+++ routes/Twitter.js
@@ -8,15 +8,53 @@ const bcrypt = require('bcrypt')
 const Twitter = require('../models/Twiter.model');
 twitters.use(cors());
 
-twitters.get('/', (req, res) => {
+// twitters.get('/', (req, res) => {
+//     Twitter.find({
+
+//     })
+//     .then(twitter=> {
+//         if(twitter){
+//             res.json(twitter);
+//         } else {
+//             res.send("Twitters does not exist")
+//         }
+//     })
+//     .catch(err => {
+//         res.send('error: ' + err)
+//     })
+// })
+
+twitters.post('/add', (req,res) => {
     Twitter.find({
 
     })
-    .then(twitter=> {
-        if(twitter){
-            res.json(twitter);
+    .then(twitter => {
+        if(twitter) {
+            const username = req.body.username;
+            const title = req.body.title;
+             const description = req.body.description;
+             const link = req.body.link;
+
+             const newTwitt = new Twitter({
+                username,
+                description,
+                title,
+                link,
+            });
+            newTwitt.save()
+            .then(()=> res.json('twitter added!!!'))
+            .catch(err=> res.status(400).json('Error: ' + err))
+        }
+    })
+})
+
+twitters.get('/:id', (req,res) => {
+    Twitter.findById(req.params.id)
+    .then(twit => {
+        if(twit){
+            res.json(twitt)
         } else {
-            res.send("Twitters does not exist")
+            res.send("Twitters id does exist")
         }
     })
     .catch(err => {
@@ -24,76 +62,38 @@ twitters.get('/', (req, res) => {
     })
 })
 
-// twitters.post('/add', (req,res) => {
-//     Twitter.find({
 
-//     })
-//     .then(twitter => {
-//         if(twitter) {
-//             const username = req.body.username;
-//             const title = req.body.title;
-//              const description = req.body.description;
-//              const link = req.body.link;
+twitters.delete('/:id', (req,res) => {
+    Twitter.findByIdAndDelete(req.params.id)
+    .then(twit => {
+        if(twit){
+            res.json(twitt)
+        } else {
+            res.send("Twitters delete does exist")
+        }
+    })
+    .catch(err => {
+        res.send('error: ' + err)
+    })
+})
 
-//              const newTwitt = new Twitter({
-//                 username,
-//                 description,
-//                 title,
-//                 link,
-//             });
-//             newTwitt.save()
-//             .then(()=> res.json('twitter added!!!'))
-//             .catch(err=> res.status(400).json('Error: ' + err))
-//         }
-//     })
-// })
+twitters.post('/updateTwitter/:id' , (req, res) => {
+    Twitter.findById(req.params.id) 
+        .then(twitt => {
+            twitt.username = req.body.username;
+            twitt.description = req.body.description;
+            twitt.title = req.body.title;
+            twitt.link = req.body.link;
 
-// twitters.get('/:id', (req,res) => {
-//     Twitter.findById(req.params.id)
-//     .then(twit => {
-//         if(twit){
-//             res.json(twitt)
-//         } else {
-//             res.send("Twitters id does exist")
-//         }
-//     })
-//     .catch(err => {
-//         res.send('error: ' + err)
-//     })
-// })
-
-
-// twitters.delete('/:id', (req,res) => {
-//     Twitter.findByIdAndDelete(req.params.id)
-//     .then(twit => {
-//         if(twit){
-//             res.json(twitt)
-//         } else {
-//             res.send("Twitters delete does exist")
-//         }
-//     })
-//     .catch(err => {
-//         res.send('error: ' + err)
-//     })
-// })
-
-// twitters.post('/updateTwitter/:id' , (req, res) => {
-//     Twitter.findById(req.params.id) 
-//         .then(twitt => {
-//             twitt.username = req.body.username;
-//             twitt.description = req.body.description;
-//             twitt.title = req.body.title;
-//             twitt.link = req.body.link;
-
-//             twitt.save()
-//             .then(()=> res.json('TwitterModel updated!'))
-//             .catch(err => {
-//                 res.send("errory: " + err)
-//             })
-//         })
-//         .catch(err => {
-//             res.send("error: " + err)
-//         });
-// })
+            twitt.save()
+            .then(()=> res.json('TwitterModel updated!'))
+            .catch(err => {
+                res.send("errory: " + err)
+            })
+        })
+        .catch(err => {
+            res.send("error: " + err)
+        });
+})
 
 module.exports = twitters;
