e3e57e545886fb353b00f188d58f659acaea49dc
szkapec <szkapec999@o2.pl>
2020-04-10 18:54:04 +0200

    change message

--- client/src/components/Chat/ComunicatorMessage.js
+++ client/src/components/Chat/ComunicatorMessage.js
@@ -104,67 +104,67 @@ const Message = (props) => {
                     <StyledAll1>{props.username[1]&&props.description[1]}</StyledAll1>
                 </div>)}
                 {props.username[2]&&(<div>
-                    <StyledName>{props.username[2]&&props.username[2]}: {props.data[2]}</StyledName>
+                    <StyledName>{props.username[2]&&props.username[2]}: {props.data[2].substr(0,10)+" "  +  props.data[2].substr(11,5) }</StyledName>
                     
                     <StyledAll>{props.username[2]&&props.description[2]}</StyledAll>
                 </div>)}
                 {props.username[3]&&(<div>
-                    <StyledName1>{props.username[3]&&props.username[3]}: {props.data[3]}</StyledName1>
+                    <StyledName1>{props.username[3]&&props.username[3]}: {props.data[3].substr(0,10)+" "  +  props.data[3].substr(11,5) }</StyledName1>
                     <StyledAll1>{props.username[3]&&props.description[3]}</StyledAll1>
                 </div>)}
                 {props.username[4]&&(<div>
-                    <StyledName>{props.username[4]&&props.username[4]}: {props.data[4]}</StyledName>
+                    <StyledName>{props.username[4]&&props.username[4]}: {props.data[4].substr(0,10)+" "  +  props.data[4].substr(11,5) }</StyledName>
                     <StyledAll>{props.username[4]&&props.description[4]}</StyledAll>
                 </div>)}
 
                 {props.username[5]&&(<div>
-                    <StyledName1>{props.username[5]&&props.username[5]}: {props.data[4]}</StyledName1>
+                    <StyledName1>{props.username[5]&&props.username[5]}: {props.data[5].substr(0,10)+" "  +  props.data[5].substr(11,5) }</StyledName1>
                     <StyledAll1>{props.username[5]&&props.description[5]}</StyledAll1>
                 </div>)}
 
                 {props.username[6]&&(<div>
-                    <StyledName>{props.username[6]&&props.username[6]}: {props.data[6]}</StyledName>
+                    <StyledName>{props.username[6]&&props.username[6]}: {props.data[6].substr(0,10)+" "  +  props.data[6].substr(11,5) }</StyledName>
                     <StyledAll>{props.username[6]&&props.description[6]}</StyledAll>
                 </div>)}
 
                 {props.username[7]&&(<div>
-                    <StyledName1>{props.username[7]&&props.username[7]}: {props.data[7]}</StyledName1>
+                    <StyledName1>{props.username[7]&&props.username[7]}: {props.data[7].substr(0,10)+" "  +  props.data[7].substr(11,5) }</StyledName1>
                     <StyledAll1>{props.username[7]&&props.description[7]}</StyledAll1>
                 </div>)}
 
                 {props.username[8]&&(<div>
-                    <StyledName>{props.username[8]&&props.username[8]}: {props.data[8]}</StyledName>
+                    <StyledName>{props.username[8]&&props.username[8]}: {props.data[8].substr(0,10)+" "  +  props.data[8].substr(11,5) }</StyledName>
                     <StyledAll>{props.username[8]&&props.description[8]}</StyledAll>
                 </div>)}
 
                 {props.username[9]&&(<div>
-                    <StyledName1>{props.username[9]&&props.username[9]}: props.data[9]}</StyledName1>
+                    <StyledName1>{props.username[9]&&props.username[9]}: {props.data[9].substr(0,10)+" "  +  props.data[9].substr(11,5) }</StyledName1>
                     
                     <StyledAll1>{props.username[9]&&props.description[9]}</StyledAll1>
                 </div>)}
 
                 {props.username[10]&&(<div>
-                    <StyledName>{props.username[10]&&props.username[10]}: {props.data[10]}</StyledName>
+                    <StyledName>{props.username[10]&&props.username[10]}: {props.data[10].substr(0,10)+" "  +  props.data[10].substr(11,5) }</StyledName>
                     <StyledAll>{props.username[10]&&props.description[10]}</StyledAll>
                 </div>)}
                 
                 {props.username[11]&&(<div>
-                    <StyledName1>{props.username[11]&&props.username[11]}: {props.data[11]}</StyledName1>
+                    <StyledName1>{props.username[11]&&props.username[11]}: {props.data[11].substr(0,10)+" "  +  props.data[11].substr(11,5) }</StyledName1>
                     <StyledAll1>{props.username[11]&&props.description[11]}</StyledAll1>
                 </div>)}
                 
                 {props.username[12]&&(<div>
-                    <StyledName>{props.username[12]&&props.username[12]}: {props.data[12]}</StyledName>
+                    <StyledName>{props.username[12]&&props.username[12]}: {props.data[12].substr(0,10)+" "  +  props.data[12].substr(11,5) }</StyledName>
                     <StyledAll>{props.username[12]&&props.description[12]}</StyledAll>
                 </div>)}
                 
                 {props.username[13]&&(<div>
-                    <StyledName1>{props.username[13]&&props.username[13]}: {props.data[13]}</StyledName1>
+                    <StyledName1>{props.username[13]&&props.username[13]}: {props.data[13].substr(0,10)+" "  +  props.data[13].substr(11,5) }</StyledName1>
                     <StyledAll1>{props.username[13]&&props.description[13]}</StyledAll1>
                 </div>)}
                 
                 {props.username[14]&&(<div>
-                    <StyledName>{props.username[14]&&props.username[14]}: {props.data[14]}</StyledName>
+                    <StyledName>{props.username[14]&&props.username[14]}: {props.data[14].substr(0,10)+" "  +  props.data[14].substr(11,5) }</StyledName>
                     <StyledAll>{props.username[14]&&props.description[14]} </StyledAll>
                 </div>)}
             </StyledCard>
